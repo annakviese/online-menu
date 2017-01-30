61e855e0b6732d83ed8ce4c44601bd57d455dbae
Anna Kviese <anna.kviese@gmail.com>
2017-01-30 14:03:26 -0800

    added miss, match criteria and a not found component

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+    render(){
+        return(
+            <h2>Sorry, there is no page like this!!!</h2>
+        )
+    }
+}
+export default NotFound;
